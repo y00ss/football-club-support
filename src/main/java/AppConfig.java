45d048277de420b7f41c2c1b0f9272e4c612b835
y00ss <youssef.dev@pm.me>
2023-02-13 13:50:41 +0100

    Fix import of annotation Value

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,13 +1,15 @@
-import com.google.api.client.util.Value;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.spanner.Spanner;
 import com.google.cloud.spanner.SpannerOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Configuration
 @ComponentScan(basePackages = "com.example.spanner_db")
@@ -17,7 +19,7 @@ public class AppConfig {
 
     @Bean
     public Spanner spanner() throws IOException {
-        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+        GoogleCredentials credentials = GoogleCredentials.fromStream(Files.newInputStream(Paths.get(credentialsPath)));
         SpannerOptions options = SpannerOptions.newBuilder().setCredentials(credentials).build();
         return options.getService();
     }
